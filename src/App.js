912014236320793b91d3cad7e67670068d34e984
Afif Islam <afifislam1123@gmail.com>
2022-03-24 17:21:30 +0530

    written into files

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import './App.css';
+import Clock from './Clock';
+
 
 function App() {
   return (
     <div className="App">
+      <Clock/>
     </div>
   );
 }
--- src/Clock.js
+++ src/Clock.js
@@ -1,18 +1,52 @@
-import React from 'react';
+import React, { useEffect, useState } from "react";
 
 const Clock = () => {
-    return (
-        <div>
-            <div className='clock'>
-                <div className='face'>
-                    <div id='secondHand' className='hand second'></div>
-                    <div id='minuteHand' className='hand minute'></div>
-                    <div id='hourHand' className='hand hour'></div>
-                    <div className='hand center'></div>
-                </div>
-            </div>
+  const [time, setTime] = useState(new Date());
+  useEffect(() => {
+    const interval = setInterval(() => setTime(new Date()), 1000);
+
+    let hr = time.getHours();
+    let min = time.getMinutes();
+    let sec = time.getSeconds();
+
+    const hourHand = document.getElementById("hourHand");
+    const minuteHand = document.getElementById("minuteHand");
+    const secondHand = document.getElementById("secondHand");
+
+    hourHand.style.transform = `rotateZ(${180 + hr * 30}deg)`;
+    // console.log(`rotateZ(${hr*30}deg)`);
+    minuteHand.style.transform = `rotateZ(${180 + min * 6}deg)`;
+    console.log(`rotateZ(${min * 6}deg)`);
+    secondHand.style.transform = `rotateZ(${sec * 6}deg)`;
+
+    return () => {
+      clearInterval(interval);
+    };
+  }, [time]);
+  return (
+    <div>
+      <div className="clock">
+        <div className="face">
+          <div id="secondHand" className="hand second"></div>
+          <div id="minuteHand" className="hand minute"></div>
+          <div id="hourHand" className="hand hour"></div>
+          <div className="hand center"></div>
+          <div className="number number1">1</div>
+          <div className="number number2">2</div>
+          <div className="number number3">3</div>
+          <div className="number number4">4</div>
+          <div className="number number5">5</div>
+          <div className="number number6">6</div>
+          <div className="number number7">7</div>
+          <div className="number number8">8</div>
+          <div className="number number9">9</div>
+          <div className="number number10">10</div>
+          <div className="number number11">11</div>
+          <div className="number number12">12</div>
         </div>
-    );
-}
+      </div>
+    </div>
+  );
+};
 
 export default Clock;
